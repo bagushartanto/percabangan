26335678c5e0a4723c22b143edb44e91f405b082
bh1990 <56873720+bh1990@users.noreply.github.com>
2019-10-22 18:53:32 +0700

    bagus hartanto
    tugas java

---
+++ Bagus/src/bagus/Bagus.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bagus;
+
+/**
+ *
+ * @author Bagus Hartanto
+ */
+public class Bagus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("=== DATA DIRI PRIBADI ===");
+           
+        String nama= " BAGUS HARTANTO";
+        String tempat= " Pasuruan,";
+        String tgl= " 02 Maret 1990";
+        String jk= " Laki - Laki";
+        String dusun= " Semendi ";
+        String rt= "Rt 03 ";
+        String rw= "Rw 05";
+        String desa= " Pulokerto";
+        String kec= " Kraton";
+        String kab= " Pasuruan";
+        String agama= " Islam";
+        String stkw= " Kawin";
+        String pekerjaan= " Karyawan Swasta";
+        String kwg= " Warga Negara Indonesia";
+        String jurusan= " Teknik Informatika";
+        long nim= 201969040028L;
+        String angk= "2019 / 2020";
+        
+        System.out.println("Nama :" +nama);
+        System.out.println("Tempat Tanggal Lahir : " +tempat +tgl);
+        System.out.println("Jenis Kelamin :" +jk);
+        System.out.println("Alamat :" +dusun +rt +rw +desa +kec +kab);
+        System.out.println("Agama :" +agama);
+        System.out.println("Status Perkawinan :" +stkw);
+        System.out.println("Pekerjaan :" +pekerjaan);
+        System.out.println("Kewarganegaraan :" +kwg);
+        System.out.println("Jurusan :" +jurusan);
+        System.out.println("NIM : " +nim);
+        System.out.println("Angkatan :" +angk);
+        System.out.println("====================================");
+    }
+    
+}
